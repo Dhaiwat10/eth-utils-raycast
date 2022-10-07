3d39de0f0be32cbdb4f172e2f5fb8e5199bad899
Tanishq Sharma <tanishqsh@gmail.com>
2022-10-07 15:54:27 -0400

    Verification of SHA256 functionality added

--- src/sha256.tsx
+++ src/sha256.tsx
@@ -1,10 +1,12 @@
 import { Action, ActionPanel, Form, showToast, Toast } from "@raycast/api";
 import { ethers } from "ethers";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Command = () => {
   const [input, setInput] = useState("");
+  const [verifyMode, setVerifyMode] = useState(false);
   const [sha256, setSha256] = useState("");
+  const [verificationHash, setVerificationHash] = useState("");
 
   const handleConvert = (val: string) => {
     try {
@@ -16,6 +18,33 @@ const Command = () => {
       showToast(Toast.Style.Failure, "Error", String(error));
     }
   };
+
+  const updateVerificationHash = (val: string) => {
+    try {
+      setVerificationHash(val);
+
+      // check if hash is valid
+      if (val.length !== 66 || val.slice(0, 2) !== "0x") {
+        showToast(Toast.Style.Failure, "Hash is not valid");
+        return;
+      }
+
+      // check if hash matches
+      if (val === sha256) {
+        showToast(Toast.Style.Success, "Hash matches");
+      } else {
+        showToast(Toast.Style.Failure, "Hash does not match");
+      }
+    } catch (error) {
+      showToast(Toast.Style.Failure, "Error", String(error));
+    }
+  };
+
+  useEffect(() => {
+    if (verifyMode) {
+      updateVerificationHash(verificationHash);
+    }
+  }, [input, verificationHash]);
 
   return (
     <Form
@@ -33,7 +62,17 @@ const Command = () => {
         value={input}
         onChange={handleConvert}
       />
-      <Form.Description title="Generated Hash" text={input.length === 0 ? "-" : sha256} />
+      <Form.Checkbox label="Verify Mode" id="verifyMode" value={verifyMode} onChange={setVerifyMode} />
+      {verifyMode && (
+        <Form.TextField
+          id="hashInput"
+          title="Enter Hash"
+          placeholder="0x7a21..."
+          value={verificationHash}
+          onChange={updateVerificationHash}
+        />
+      )}
+      {!verifyMode && <Form.Description title="Generated Hash" text={input.length === 0 ? "-" : sha256} />}
     </Form>
   );
 };
