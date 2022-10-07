41f82d1fcf6fe5d2da2c1b03b57960adb7ac5c0f
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-10-07 19:26:54 +0700

    feat: add hexstring converter

---
+++ src/hexstring.tsx
@@ -0,0 +1,49 @@
+import { Form } from "@raycast/api";
+import { useState } from "react";
+import { BigNumber as BN } from "ethers";
+
+const Command = () => {
+  const [hexValue, setHexValue] = useState("");
+  const [decimalValue, setDecimalValue] = useState("");
+
+  const handleConvert = (val: string, sentFrom: "hex" | "decimal") => {
+    try {
+      if (val === "") {
+        setHexValue("");
+        setDecimalValue("");
+        return;
+      }
+
+      if (sentFrom === "hex") {
+        setHexValue(val);
+        setDecimalValue(BN.from(val).toString());
+      } else {
+        setDecimalValue(val);
+        setHexValue(BN.from(val).toHexString());
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Form>
+      <Form.TextField
+        id="hexValue"
+        title="Hex Value"
+        placeholder="0x123..."
+        value={hexValue}
+        onChange={(val) => handleConvert(val, "hex")}
+      />
+      <Form.TextField
+        id="decimalValue"
+        title="Decimal Value"
+        placeholder="123..."
+        value={decimalValue}
+        onChange={(val) => handleConvert(val, "decimal")}
+      />
+    </Form>
+  );
+};
+
+export default Command;
